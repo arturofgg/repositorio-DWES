17a9d461012c724ca9566337128462bd19240271
arturofgg <arturofdezgg17@gmail.com>
2022-10-05 21:48:34 +0200

    hash oct 5

---
+++ DWES 22-23/Ejercicios/ut02/Ejercicios de compañeros/hash.php
@@ -0,0 +1,14 @@
+<?php
+//$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72
+for($a=97; $a<123; $a++ ){
+    for($b=97; $b<123;$b++ ){
+        for($c=97; $c<123;$c++ ){
+            for($d=97; $d<123; $d++ ){
+                if(password_verify(chr($d).chr($c).chr($b).chr($a),'$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72')==1){
+                    echo("si " . chr($d).chr($c).chr($b).chr($a));
+                }else echo("no");
+            }
+        }
+    }
+}
+?>
