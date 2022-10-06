eaf27f8a86112fd25b602a401b070f1bb743fb0a
arturofgg <arturofdezgg17@gmail.com>
2022-10-06 19:41:07 +0200

    6 oct

--- DWES 22-23/Ejercicios/ut02/Ejercicios de compañeros/3.php
+++ DWES 22-23/Ejercicios/ut02/Ejercicios de compañeros/3.php
@@ -1,8 +1,17 @@
 <?php/*
 3 Xing , Marcos
-Funciones: array_replace, array_walk_recursive.
-Enunciado: Crear una array bidimesional que guarda nombre de usuario y contraseña de usuario
-con array_walk_recursive ejecuta una funcion predefinida mostrando nombre de usuario y contraseña, despues con boton cambiar constraseña puede hace cambia de contraseña usando array_replace.*/
+Utiliza la función print_r() para ver la evolución de cada array.
+Funciones: array_walk, array_map, array_replace
+Extra: https://www.php.net/manual/es/function.password-hash.php
+$usuarios = [
+	"jorge" => "1234",
+	"amparo" => "admin",
+	"mary" = > "",
+]
+Enunciado: Crea una array bidimesional que guarda nombre de usuario y contraseña de usuario en texto claro. array_walk ejecuta una funcion predefinida mostrando nombre de usuario y contraseña
+Enunciado: Utilizando las funciones de contraseñas y la función array_map. Genera un array nuevo con los usuarios y su contraseña en formato hash.
+Enunciado: En base al ejercicio anterior cambia la función para que los usuarios sin contraseña tenga la contraseña "tmp2022"
+Enunciado: Haz un filtrado de usuarios sin contraseña, utiliza array_replace para establecer en el array original $usuariosla contraseña de los usuarios que no tenían.*/
 ?>
 
 <?php 
--- DWES 22-23/Ejercicios/ut02/Ejercicios de compañeros/hash.php
+++ DWES 22-23/Ejercicios/ut02/Ejercicios de compañeros/hash.php
@@ -5,7 +5,7 @@ for($a=97; $a<123; $a++ ){
         for($c=97; $c<123;$c++ ){
             for($d=97; $d<123; $d++ ){
                 if(password_verify(chr($d).chr($c).chr($b).chr($a),'$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72')==1){
-                    echo("si " . chr($d).chr($c).chr($b).chr($a));
+                    echo(chr($d).chr($c).chr($b).chr($a));
                 }
             }
         }
