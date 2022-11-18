1dca8adf2a9f0e44db5cf75d8cc702d37e7179a2
arturofgg <arturofdezgg17@gmail.com>
2022-11-18 18:35:49 +0100

    bases_datos

--- DWES 22-23/Ejercicios/ut04/bases_datos/b1.php
+++ DWES 22-23/Ejercicios/ut04/bases_datos/b1.php
@@ -1,46 +1,21 @@
+<!--
 PDO
 =========================================
 testConnectionPDO.php
+-->
 <?php
 
 try {
-    $mbd = new PDO('mysql:host=localhost;dbname=proyecto01_juan_de_la_cierva', "juan", "cierva1234");
+    $mbd = new PDO('mysql:host=localhost;dbname=proyectoEventos', "arturo", "arturo12345");
 
     // Utilizar la conexión aquí
-    $resultado = $mbd->query('SELECT * FROM cosas');
+    $resultado = $mbd->query('SELECT * FROM Ciclistas');
 
     foreach ($resultado as $fila){
       foreach ($fila as $clave => $valor){
         echo $clave . " " . $valor . "\n";
       }
-      echo "--------------\n"
-    }
-
-    // Ya se ha terminado; se cierra
-    $resultado = null;
-    $mbd = null;
-
-} catch (PDOException $e) {
-    print "¡Error!: " . $e->getMessage() . "\n";
-    die();
-}
-
-?>PDO
-=========================================
-testConnectionPDO.php
-<?php
-
-try {
-    $mbd = new PDO('mysql:host=localhost;dbname=proyecto01_juan_de_la_cierva', "juan", "cierva1234");
-
-    // Utilizar la conexión aquí
-    $resultado = $mbd->query('SELECT * FROM cosas');
-
-    foreach ($resultado as $fila){
-      foreach ($fila as $clave => $valor){
-        echo $clave . " " . $valor . "\n";
-      }
-      echo "--------------\n"
+      echo "<br>";
     }
 
     // Ya se ha terminado; se cierra
@@ -53,3 +28,8 @@ try {
 }
 
 ?>
+<!--
+PDO
+=========================================
+testConnectionPDO.php
+-->
