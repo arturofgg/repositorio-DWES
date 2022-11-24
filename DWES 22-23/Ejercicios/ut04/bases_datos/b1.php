b47bb72354bc4f49fcfc4ea26cc907a044369dcb
arturofgg <arturofdezgg17@gmail.com>
2022-11-24 21:42:44 +0100

    ejercicio bases empezado

--- DWES 22-23/Ejercicios/ut04/bases_datos/b1.php
+++ DWES 22-23/Ejercicios/ut04/bases_datos/b1.php
@@ -1,51 +1,65 @@
-<!--
-PDO
-=========================================
-testConnectionPDO.php
--->
 <?php
 
-try {
-    $mbd = new PDO('mysql:host=localhost;dbname=proyectoEventos', "arturo", "arturo12345");
+function pintarDatos()
+{
+    try {
+        $mbd = new PDO('mysql:host=localhost;dbname=proyectoEventos', "arturo", "arturo12345");
+        $stmt = $mbd->prepare("SELECT * FROM Ciclistas");
+        $stmt->execute();
+        $cosas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        //RECORRE LOS CAMPOS QUE ELIJAS
+?>
+        <table>
+            <?php foreach ($cosas as $cliente) { ?>
+                <tr>
+                    <?= "<td> " . $cliente['id'] . "</td><td><a href='b1redirect.php'> " . $cliente['nombre'] . "</a></td><td> " . $cliente['num_trofeos'] . "</td>"; ?>
+                </tr>
+            <?php } ?>
+        </table>
+<?php
 
-
-    $stmt = $mbd->prepare("SELECT * FROM Ciclistas");
-$stmt->execute();
-$clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach($clientes as $cliente){
-    echo $cliente['nombre'] . "<br>";
-}
-
-
-
-
-
-
-
-
-    // Utilizar la conexión aquí
-    $resultado = $mbd->query('SELECT * FROM Ciclistas');
-
-    foreach ($resultado as $fila){
-      foreach ($fila as $clave => $valor){
-        echo $clave . " " . $valor . "\n";
-      }
-      echo "<br>";
+        /* RECORRE TODOS LOS CAMPOS SIN SALTARSE NINGUNO
+    
+        $acc = "";
+        foreach ($cosas as $cliente) {
+            foreach ($cliente as $campo) {
+                $acc .= $campo . " ";
+            }
+            $acc .= "<br>";
+        }
+        echo $acc;
+    
+    */
+        // Ya se ha terminado; se cierra
+        $cosas = null;
+        $mbd = null;
+    } catch (PDOException $e) {
+        print "¡Error!: " . $e->getMessage() . "\n";
+        die();
     }
-
-    // Ya se ha terminado; se cierra
-    $resultado = null;
-    $mbd = null;
-
-} catch (PDOException $e) {
-    print "¡Error!: " . $e->getMessage() . "\n";
-    die();
 }
 
 ?>
-<!--
-PDO
-=========================================
-testConnectionPDO.php
--->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>b1</title>
+    <style>
+        table, td , tr{
+            border-collapse: collapse;
+            border:  1px solid black;
+        } 
+        td{
+            padding: 3px;
+        }
+    </style>
+</head>
+<body>
+    <?php pintarDatos() ?>
+</body>
+</html>
