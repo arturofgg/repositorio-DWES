90cfa655b1c1ec9b5e6bfb5da22e4528e755df54
arturofgg <arturofdezgg17@gmail.com>
2022-11-29 12:29:41 +0100

    c

--- UTILIDADEXAMEN/Generales.php
+++ UTILIDADEXAMEN/Generales.php
@@ -1,7 +1,41 @@
 <?php
 namespace UTILIDADEXAMEN;
 
-//Hacer un numero primo
+/*CON EL $_GET pillo EL VALOR DEL NAME DEL INPUT! 
+  (ut01/0-3-ejercicios/e-hechos2)*/
+
+
+//SACAR UNA LETRA ALEATORIA
+chr(rand(ord("a"), ord("z")));
+
+
+//SACAR UN NUMERO ALEATORIO DEL 0 AL 7
+rand(0,7);
+
+
+//SACAR VOCALES Y CONSONANTES DE UNA CADENA
+for($i=0; $i<strlen($text); $i++) {
+    $letra=$text[$i];
+        if($letra == 'a' || $letra == 'e' || $letra == 'i' || $letra == 'o' || $letra == 'u'){
+            $vocales++;
+        }else if($letra == ' ' || $letra == '?'|| $letra == '!'|| $letra == '¿' || $letra == '¡'|| $letra == '+'|| $letra == '-' || $letra == '*'|| $letra == '/'|| $letra == ':'|| $letra == ';'|| $letra == '_'|| $letra == '('|| $letra == ')'|| $letra == '&'|| $letra == '%'|| $letra == '#' || $letra == '@'|| $letra == 'º'|| $letra == '['|| $letra == ']'|| $letra == '{'|| $letra == '}'|| $letra == '~'){
+        }else{
+            $consonantes++;
+        }
+}
+
+
+//PALINDROMO
+for($j=0; $j<strlen($text)/2 && $palindromo; $j++){
+    $letra1=$text[$j];
+    $letra2=$text[strlen($text)-$j-1];
+    if($letra1 !== $letra2){
+        $palindromo=false;
+    }
+}
+
+
+//SACAR UN NUMERO PRIMO
 function esPrimo($n){
     if ($n == 0 || $n == 1){
         return true;
@@ -18,8 +52,5 @@ function esPrimo($n){
     }
 }
 
-//Sacar una letra aleatoria
-chr(rand(ord("a"), ord("z")));
 
-//Sacar un numero aleatorio
 ?>
